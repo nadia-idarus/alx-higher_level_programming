dc1f7966a74f8ce467399584ba2ddf64ab0e47fc
nadia-idarus <nadia.abdalla@riarauniversity.ac.ke>
2024-01-22 08:43:21 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,11 +4,8 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end=' ')
     except IndexError:
-        pass  # Handles the case where x is greater than the length of the list
+        pass  
     finally:
-        print()  # Adds a newline after printing the elements
-
-# Example usage:
-my_list = [1, 2, 3, 4, 5]
-safe_print_list(my_list, 3)
-
+        print("")
+        
+        return total
